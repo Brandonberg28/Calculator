afc62f69993e9086a56fd0922bfab1a2969a4e85
Steve Beckert <stevebeckert@hotmail.com>
2020-05-13 15:12:57 -0700

    Added maven javafx project with JUnit5

---
+++ src/test/java/org/brandonberg28/MathCalculatorAppTest.java
@@ -0,0 +1,20 @@
+package org.brandonberg28;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MathCalculatorAppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
