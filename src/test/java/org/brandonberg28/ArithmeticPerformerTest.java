02fae69737a081137187aa2668f83da8ec859ec2
Brandon Bergado <brandon.bergado@gmail.com>
2020-05-14 21:01:21 -0700

    Added tests

--- src/test/java/org/brandonberg28/ArithmeticPerformerTest.java
+++ src/test/java/org/brandonberg28/ArithmeticPerformerTest.java
@@ -1,7 +1,5 @@
 package org.brandonberg28;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
@@ -10,9 +8,7 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class ArithmeticPerformerTest {
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
     public void addTest()
     {
@@ -26,10 +22,56 @@ public class ArithmeticPerformerTest {
         actual = ArithmeticPerformer.add(num1, num2);
 
         //assert
-        assertEquals(expected, actual, "messageSupplier");
-        //assertTrue(expected==actual);
-        
+        assertEquals(expected, actual);
+        //assertTrue(expected==actual);  
+    }
+
+    @Test
+    public void subtractTest()
+    {
+        //arrange
+        int num1 = 20;
+        int num2 = 18;
+        int expected = num1 - num2;
+        int actual;
+
+        //act
+        actual = ArithmeticPerformer.subtract(num1, num2);
+
+        //assert
+        assertEquals(expected, actual);
     }
     
+    @Test
+    public void multiplyTest()
+    {
+        //arrange
+        int num1 = 4;
+        int num2 = 2;
+        int expected = num1 * num2;
+        int actual;
+
+        //act
+        actual = ArithmeticPerformer.multiply(num1, num2);
+
+        //assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void divideTest()
+    {
+        //arrange
+        int num1 = 10;
+        int num2 = 5;
+        int expected = num1 / num2;
+        int actual;
+
+        //act
+        actual = ArithmeticPerformer.divide(num1, num2);
+
+        //assert
+        assertEquals(expected, actual);
+    }
 
 }
